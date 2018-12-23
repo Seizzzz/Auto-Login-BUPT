c141ef8eaf9c3cf7abb6a7157f0028b5b623a586
SLRYZD <suozhengduo@hotmail.com>
2018-12-23 21:07:14 +0800

    Create login2.0.py
    1. 追加了动态获取sessionid的功能，防止session失效后需要重复修改
    2. 将需要修改的部分提前，希望更为易用

---
+++ Python/login2.0.py
@@ -0,0 +1,38 @@
+import requests
+
+user = ' '  #账号
+passwd = ' ' #密码
+clientcookie = ' '   #此处为浏览器cookie
+
+
+
+###以下内容无需修改###
+url = 'http://ngw.bupt.edu.cn/login'
+data={'user': user,
+      'pass': passwd,
+      'line': 'CUC-BRAS'} #联通线路
+
+def getcookie():
+    session = requests.session()
+    result = session.post(url,data=data)
+    cookies = requests.utils.dict_from_cookiejar(session.cookies)
+    return str(cookies)[15:31]
+
+def login():
+    postdata = data
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+               'Accept-Encoding': 'gzip, deflate',
+               'Accept-Language': 'zh-CN,zh;q=0.9',
+               'Cache-Control': 'max-age=0',
+               'Connection': 'keep-alive',
+               'Content-Length': '44',
+               'Content-Type': 'application/x-www-form-urlencoded',
+               'Cookie': '%s; SessionId=%s' %(clientcookie,getcookie()),
+               'Host': 'ngw.bupt.edu.cn',
+               'Origin': 'http://ngw.bupt.edu.cn',
+               'Referer': 'http://ngw.bupt.edu.cn/index',
+               'Upgrade-Insecure-Requests': '1',
+               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36'}
+    requests.post(url, data=postdata)
+
+login()
