38692f679d3034e405cf1ebf047fa81c300b6e80
SLRYZD <suozhengduo@hotmail.com>
2019-03-30 13:51:06 +0800

    Update login3.0.py

--- Python/login3.0.py
+++ Python/login3.0.py
@@ -5,15 +5,15 @@ passwd = ' ' #密码
 
 ###以下内容无需修改###
 url0 = 'http://10.3.8.216/login'
-url1 = 'http://10.3.8.217/login'
-url2 = 'http://10.3.8.217/index'
+url1 = 'http://10.3.8.217/index'
+url2 = 'http://10.3.8.217/login'
 
+data0 = {'user': user,
+        'pass': passwd}
 data1 = {'user': user,
         'pass': passwd,
         'line': 'CUC-BRAS'} #联通线路
-data2 = {'user': user,
-        'pass': passwd}
 
-requests.post(url0, data2)
-requests.post(url2, data2)
+requests.post(url0, data0)
 requests.post(url1, data1)
+requests.post(url2, data1)
